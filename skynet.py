521fc3b1e444a2e138ac26efe5cf9cc2e29f44b4
Jiayi Tang <jiayitang10@gmail.com>
2019-09-30 22:45:32 -0600

    Redid row reading alg

--- skynet.py
+++ skynet.py
@@ -16,17 +16,18 @@ def greyMatrixFromPath(path):
 
 def getRows(mat):
     rows = []
-    i = 0
-    while i < mat.shape[0]:
-        if np.mean(mat[i]) != 255:
-            rowMat = mat[i]
-            i += 1
-            while np.mean(mat[i]) != 255:
-                rowMat = np.vstack((rowMat, mat[i]))
-                i += 1
-            rows.append(pytesseract.image_to_string(rowMat))                
+    row = None
+    for i in range(mat.shape[0]):
+        if np.mean(mat[i]) == 255:
+            if row is not None:
+                rows.append(pytesseract.image_to_string(row))
+                row = None
         else:
-            i += 1
+            if row is None:
+                row = mat[i]
+            else:
+                row = np.vstack((row, mat[i]))
+
     return rows
 
 

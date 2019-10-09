8f8969c6cd83f863302ff255291a8d3dba27e86d
Jiayi Tang <jiayitang10@gmail.com>
2019-10-08 19:22:10 -0600

    Finished merging duped schools in db

--- dbmerge.py
+++ dbmerge.py
@@ -1,8 +1,24 @@
 from peewee import *
 from tabulanet import School, Swimmer
 
-def mergeSchools(fromname, toname):
-    fromSchool = School.get(name = fromname)
-    toSchool = School.get(name = toname)
+def mergeSchools(fromSchool, toSchool):
+    # fromSchool = School.get(name = fromname)
+    # toSchool = School.get(name = toname)
 
+    query = Swimmer.update(school = toSchool).where(Swimmer.school == fromSchool)
+    numRowsChanged = query.execute()
     
+    fromSchool.delete_instance()
+    print("{} entries changed".format(numRowsChanged))
+
+if __name__ == "__main__":
+    for fromSchool in School.select():
+        if "High School" in fromSchool.name:
+            try:
+                toSchoolName = fromSchool.name.replace(" High School", "")
+                toSchool = School.get(name = toSchoolName)
+                print(fromSchool.name, toSchoolName)
+
+                mergeSchools(fromSchool, toSchool)
+            except DoesNotExist:
+                print("No matching shorter name for {}".format(fromSchool.name))

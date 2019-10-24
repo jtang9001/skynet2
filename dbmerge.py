7c6f34bcd1b531771c10d14e16331249ccaadf1e
Jiayi Tang <jiayitang10@gmail.com>
2019-10-23 21:00:42 -0600

    Merged schools

--- dbmerge.py
+++ dbmerge.py
@@ -1,24 +1,37 @@
 from peewee import *
-from tabulanet import School, Swimmer
+from tabulabuilder import School, Swimmer, RelayResult
 
 def mergeSchools(fromSchool, toSchool):
     # fromSchool = School.get(name = fromname)
     # toSchool = School.get(name = toname)
 
     query = Swimmer.update(school = toSchool).where(Swimmer.school == fromSchool)
-    numRowsChanged = query.execute()
+    numSwimmersChanged = query.execute()
+
+    query = RelayResult.update(school = toSchool).where(RelayResult.school == fromSchool)
+    numRelaysChanged = query.execute()
     
     fromSchool.delete_instance()
-    print("{} entries changed".format(numRowsChanged))
+    print("{} entries changed".format(numSwimmersChanged + numRelaysChanged))
+
+def mergeSchoolsByID(fromID, toID):
+    fromSchool = School.get_by_id(fromID)
+    toSchool = School.get_by_id(toID)
+
+    print(fromSchool.name, toSchool.name)
+
+    mergeSchools(fromSchool, toSchool)
+
 
 if __name__ == "__main__":
-    for fromSchool in School.select():
-        if "High School" in fromSchool.name:
-            try:
-                toSchoolName = fromSchool.name.replace(" High School", "")
-                toSchool = School.get(name = toSchoolName)
-                print(fromSchool.name, toSchoolName)
+    mergeSchoolsByID(24, 33)
+    # for fromSchool in School.select():
+    #     if "School" in fromSchool.name:
+    #         try:
+    #             toSchoolName = fromSchool.name.replace(" School", "")
+    #             toSchool = School.get(name = toSchoolName)
+    #             print(fromSchool.name, toSchoolName)
 
-                mergeSchools(fromSchool, toSchool)
-            except DoesNotExist:
-                print("No matching shorter name for {}".format(fromSchool.name))
+    #             mergeSchools(fromSchool, toSchool)
+    #         except DoesNotExist:
+    #             print("No matching shorter name for {}".format(fromSchool.name))
